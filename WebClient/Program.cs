63a473fc26b23aa467020154d04779ffcec0574f
Hanna <57458439+HannaBuzovskaya@users.noreply.github.com>
2020-08-15 14:11:32 +0300

    Add files via upload

---
+++ WebClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace WebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+               string host = "www.google.com";
+               TcpClient client = new TcpClient();
+               client.Connect(host, 443);
+
+               SslStream sslStream = new SslStream(
+                   client.GetStream(),
+                   false,
+                   new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                   null
+               );
+
+               sslStream.AuthenticateAsClient("");
+               sslStream.ReadTimeout = 2000;
+
+               StringBuilder dataCompiler = new StringBuilder();
+               dataCompiler.AppendLine("GET / HTTP/1.1");
+               dataCompiler.AppendLine($"Host: {host}");
+               dataCompiler.AppendLine("Accept: text/html");
+               dataCompiler.AppendLine("Connection: close");
+               dataCompiler.AppendLine($"User-Agent: {Assembly.GetExecutingAssembly().GetName().Name}");
+               dataCompiler.AppendLine("");
+
+               string requestData = dataCompiler.ToString();
+
+               sslStream.Write(Encoding.UTF8.GetBytes(requestData));
+               Console.WriteLine(requestData);
+
+               var reader = new StreamReader(sslStream, Encoding.UTF8);
+
+               Console.WriteLine("TCP Received:");
+               Console.WriteLine(reader.ReadToEnd());
+
+               reader.Close();
+               client.Close();
+           }
+           catch (Exception e)
+           {
+               Console.WriteLine(e.ToString());
+           }
+        }
+        private static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
---
+++ WebServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddr, 7777);
+
+                Console.WriteLine("TCP Start:");
+                server.Start();
+                while (true)
+                {
+                Console.WriteLine("TCP Wait for a client:");
+                TcpClient client = server.AcceptTcpClient();
+
+                NetworkStream stream= client.GetStream();
+
+                string response = "Hello from the server";
+                byte [] data = Encoding.UTF8.GetBytes(response);
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Send: {0}", response);
+
+                stream.Close();
+                client.Close();
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (server != null)
+                server.Stop();
+        }
+    }
+}
+}
